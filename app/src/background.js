5ad715c5dda13805ee3cd41bf3c2a4245936324f
Ahmed Sabry <abou7mied.soft@gmail.com>
2022-01-16 18:27:18 +0200

    Update logic for extracting photos URLs

--- app/src/background.js
+++ app/src/background.js
@@ -1,12 +1,18 @@
-chrome.webRequest.onBeforeSendHeaders.addListener((details) => {
-    for (let i = 0; i < details.requestHeaders.length; i++) {
-      if (details.requestHeaders[i].name === 'Origin') {
-        details.requestHeaders[i].value = 'https://www.facebook.com';
-        break;
-      }
-    }
-    return { requestHeaders: details.requestHeaders };
-  },
-  { urls: ['https://www.facebook.com/ajax/photos/snowlift/menu*'] },
-  ['blocking', 'requestHeaders'],
-);
+// chrome.webRequest.onBeforeSendHeaders.addListener((details) => {
+//   // console.log('details', details);
+//   for (let i = 0; i < details.requestHeaders.length; i++) {
+//     if (details.requestHeaders[i].name === 'Origin') {
+//       details.requestHeaders[i].value = 'https://www.facebook.com';
+//       break;
+//     }
+//   }
+//   return { requestHeaders: details.requestHeaders };
+// // }, { urls: ['https://www.facebook.com/*'] }, ['blocking', 'requestHeaders']);
+// }, { urls: ['https://www.facebook.com/*'] }, ['blocking', 'requestHeaders']);
+//
+//
+// chrome.webRequest.onCompleted.addListener(function (details) {
+//   console.log('onCompleted details', details);
+// }, { urls: ['https://www.facebook.com/*'] });
+// // }, { urls: ['https://static.xx.fbcdn.net/rsrc.php*'] });
+// //
--- app/src/content.js
+++ app/src/content.js
@@ -1,10 +1,14 @@
 import { app } from './app';
 import async from 'async';
+import getPhotoData from './getPhotoData';
 
 const urls = [];
 const photosIds = [];
 let prevScrollHeight;
 const getUrlPromises = [];
+let docId;
+const dtsg = dtsgMatcher();
+let photoSetId;
 
 function clear() {
   photosIds.splice(0);
@@ -17,6 +21,11 @@ function init() {
     .append(results);
   app.$mount(results[0]);
   clear();
+
+  loadScriptAndExtractDocId();
+
+  console.log('dtsg', dtsg);
+
   setInterval(() => {
     if (prevScrollHeight !== document.documentElement.scrollHeight) {
       prevScrollHeight = document.documentElement.scrollHeight;
@@ -31,7 +40,7 @@ function getUrlsOf(array) {
   }
 }
 
-function getJsonFromUrl(search) {
+function parsePhotoUrlParams(search) {
   const query = search.substr(1);
   const result = {};
   query.split('&')
@@ -42,7 +51,12 @@ function getJsonFromUrl(search) {
   return result;
 }
 
-function download({ node, text, postLink, textAtRight }) {
+function download({
+                    galleryWrapperNode,
+                    text,
+                    postLink,
+                    textAtRight,
+                  }) {
   clear();
   app.init({
     text,
@@ -50,34 +64,40 @@ function download({ node, text, postLink, textAtRight }) {
     postLink: `https://www.facebook.com${postLink}`,
   });
 
-  const a = node.find('a[rel=theater]');
-  let set;
+  // const a = galleryWrapperNode.find('a[rel=theater]');
+  const a = galleryWrapperNode.find('a[role="link"]');
+
   let mapped = a.toArray()
     .map((item) => {
       if (item.href.indexOf('/posts/') !== -1 || item.href.indexOf('/photos/') !== -1) {
         const paths = item.href.split('/');
-        set = paths[paths.length - 3];
+        photoSetId = paths[paths.length - 3];
         return paths[paths.length - 2];
       }
-      const json = getJsonFromUrl(item.search);
-      set = json.set;
-      return json.fbid;
+      const photoUrlParams = parsePhotoUrlParams(item.search);
+      console.log('photoUrlParams', photoUrlParams);
+      photoSetId = photoUrlParams.set;
+      return photoUrlParams.fbid;
     });
 
+  console.log('mapped', mapped);
   getUrlsOf(mapped);
+
   async.series([
     (next) => {
       async.doDuring(
         async (callback) => {
-          const newIds = await getNextOf(mapped[mapped.length - 1], set);
-          const filtered = newIds.filter(n => mapped.indexOf(n) !== -1);
-          mapped = mapped.concat(newIds);
-          mapped = mapped.filter((item, pos) => mapped.indexOf(item) === pos);
+          const newId = await getNextOf(mapped[mapped.length - 1], photoSetId);
+          const reachedTheEnd = !newId || mapped.find(n => n.toString() === newId);
+          if (newId) {
+            mapped.push(newId);
+            mapped = mapped.filter((item, pos) => mapped.indexOf(item) === pos);
+          }
           getUrlsOf(mapped);
-          callback(null, filtered);
+          callback(null, reachedTheEnd);
         },
-        (filtered, callback) => {
-          callback(null, !filtered.length);
+        (reachedTheEnd, callback) => {
+          callback(null, !reachedTheEnd);
         },
         () => {
           next();
@@ -92,35 +112,68 @@ function download({ node, text, postLink, textAtRight }) {
 }
 
 function detectTimeNodes() {
-  const timeNode = $('._5u5j .fsm.fwn.fcg');
-  timeNode.each((i, x) => {
-    const closest = $(x)
-      .closest('._1dwg');
-    const textNode = closest.find('.userContent')[0];
+  // The time Node e.g. "2h"
+  const timeNode = document.querySelectorAll('span.j1lvzwm4.stjgntxs.ni8dbmo4.q9uorilb.gpro0wi8');
+  console.log('timeNode', timeNode);
+  timeNode.forEach((node) => {
+    const closest = $(node)
+      .closest('.j83agx80.l9j0dhe7.k4urcfbm');
+
+    console.log('closest', closest);
+
+    const textNode = closest.find('.d2edcug0.hpfvmrgz.qv66sw1b.c1et5uql.b0tq1wua.a8c37x1j.keod5gw0.nxhoafnm.aigsh9s9.d9wwppkn.fe6kdd0r.mau55g9w.c8b282yb.hrzyx87i.jq4qci2q.a3bd9o3v.b1v8xokw.oo9gr5id.hzawbc8m')[0];
+    console.log('textNode', textNode);
+
+    if (!textNode) {
+      return;
+    }
+
     const text = textNode && (textNode.innerText || textNode.textContent);
-    const textAtRight = closest.find('.userContent p')
-      .eq(0)
-      .css('text-align') === 'right';
-    const _2a2q = closest.find('._2a2q');
-    const postLink = closest.find('a._5pcq')
+
+    // const textAtRight = closest.find('.userContent p')
+    //   .eq(0)
+    //   .css('text-align') === 'right';
+    const textAtRight = false;
+
+    const galleryWrapperNode = $($(textNode.closest('.ecm0bbzt.hv4rvrfc.ihqw7lf3.dati1w0a'))
+      .parent())
+      .next();
+
+    const image = galleryWrapperNode.find('img');
+    // const galleryWrapperNode = closest.find('.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.g5gj957u.rj1gh0hx.buofh1pr.hpfvmrgz.i1fnvgqd.gs1a9yip.owycx6da.btwxx1t3.jb3vyjys');
+
+    console.log('galleryWrapperNode', galleryWrapperNode);
+
+    const postLink = $(node)
+      .closest('a')
       .attr('href');
-    if (closest && _2a2q.length) {
+
+    console.log('node', $(node));
+    console.log('link', $(node)
+      .closest('a'));
+    console.log('link', $(node)
+      .closest('a')
+      .attr('href'));
+
+    // if (closest && galleryWrapperNode.length) {
+    if (closest && image.length) {
       const aNodeParent = $('<span class="download"></span>');
-      const aNode = $('<a href=\'#\'>Download</a>');
+      const aNode = $('<a>Download</a>');
       aNodeParent.append(' ');
       aNodeParent.append(aNode);
       aNodeParent.click(() => download({
-        node: _2a2q,
+        galleryWrapperNode,
         text,
         postLink,
         textAtRight,
       }));
-      $(x)
-        .parent()
+      const targetToAddButton = $($(node)
+        .closest('a')
+        .parent());
+      targetToAddButton
         .find('.download')
         .remove();
-      $(x)
-        .parent()
+      targetToAddButton
         .append(aNodeParent);
     }
   });
@@ -129,53 +182,25 @@ function detectTimeNodes() {
 init();
 
 async function getNextOf(id, set) {
-  const data = { fbid: `${id}` };
-  if (set) {
-    data.set = set;
-  }
-  const fbDtsgAg = $('html')
-    .html()
-    .match(/async_get_token"\s*:\s*"(.*?)"/)[1];
-  return new Promise((resolve, reject) => {
-    $.ajax({
-      url: `https://www.facebook.com/ajax/pagelet/generic.php/PhotoViewerInitPagelet?data=${
-        encodeURIComponent(JSON.stringify(data))}&__a=1&fb_dtsg_ag=${encodeURIComponent(fbDtsgAg)}`,
-      type: 'GET',
-      dataType: 'text',
-      error: (err) => {
-        reject(err);
-      },
-      success: (result) => {
-        const myRegexp = /(?="addPhotoFbids")(.*?)(],\[\[)(.*?)(])/g;
-        const match = myRegexp.exec(result);
-        const ids = `[${match[3]}]`;
-        resolve(JSON.parse(ids));
-      },
-    });
+  const results = await getPhotoData({
+    fb_dtsg: dtsg,
+    docId,
+    photoId: id,
+    photoSetId,
   });
+  return results.nextImageId;
 }
 
 
-function getOriginalUrl(id) {
-  return new Promise((resolve, reject) => {
-    $.ajax({
-      url: `https://www.facebook.com/ajax/photos/snowlift/menu/?fbid=${id}`,
-      type: 'POST',
-      data: {
-        __a: '1',
-        fb_dtsg: $('input[name=fb_dtsg]')
-          .val(),
-      },
-      dataType: 'text',
-      error: (err) => reject(err),
-      success: (data) => {
-        const matched = data.match(/"download_photo","href":"(.*?)"/);
-        let finalResult;
-        if (matched) finalResult = matched[1].replace(/\\/g, '');
-        resolve(finalResult);
-      },
-    });
+async function getOriginalUrl(id) {
+  const results = await getPhotoData({
+    fb_dtsg: dtsg,
+    docId,
+    photoId: id,
+    photoSetId,
   });
+  console.log('getPhotoData results', results);
+  return results.image.uri;
 }
 
 
@@ -187,4 +212,32 @@ function addPhotoId(id, i) {
   photosIds.push(id);
   getUrlPromises.push(getOriginalUrl(id)
     .then(url => urls[i] = url));
+}
+
+function loadScriptAndExtractDocId() {
+  $.ajax({
+    url: 'https://static.xx.fbcdn.net/rsrc.php/v3/yi/r/qZ-coEUYuSR.js',
+    method: 'GET',
+    dataType: 'text',
+  })
+    .done((response) => {
+      const match = response.match(/CometPhotoRootContentQuery_facebookRelayOperation.*?e\.exports="(.*?)"/i);
+      if (match) {
+        docId = match[1];
+        console.log('docId', docId);
+      }
+    });
+}
+
+
+function findScriptTextIncludes(matcher) {
+  return $('script')
+    .toArray()
+    .find(script => matcher(script.text)).text;
+}
+
+function dtsgMatcher() {
+  const scriptText = findScriptTextIncludes(text => text.match('DTSGInitialData'));
+  const match = scriptText.match(/DTSGInitialData.*?"token":"(.*?)"/i);
+  return match[1];
 }
